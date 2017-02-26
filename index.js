c1a20e2ea2f3ad4d88281ccd4282f5d58416425e
Eidher Escalona <eescalona@carqualifier.com>
2017-02-26 00:23:18 -0500

    update

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
-var email = require('./email');
+let weather = require('./weather');
 
-console.log("Hello World!");
+weather.send();
+//console.log('Testing!');
+//var schedule = require('node-schedule');
+//let email = require('./email');
+
+// setup email data with unicode symbols
+
+//let mailOptions = {
+//    from: '"Stockanator" <me@stockanator.com>', // sender address
+//    to: 'eidher.escalona@gmail.com', // list of receivers
+//    subject: 'Hello There ✔', // Subject line
+//    text: 'Hello world ?', // plain text body
+//    html: '<b>Hola, Soy Stockanator y voy a hacerte millonario!</b>' // html body
+//};
+
+//let myEmail = email(mailOptions)
+//myEmail.send();
+
+//var j = schedule.scheduleJob('*/1 * * * *', function(){
+//  console.log('The answer to life, the universe, and everything!');
+//});
 
